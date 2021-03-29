35288f8a41324c8b9aad92b61866e47f22b280d7
Brahvim <69293652+Brahvim@users.noreply.github.com>
2021-03-29 16:24:15 +0530

    Changes to JS because something doesn't work :/

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 var p,  //p is the prompt, 
     t,  //t is the text box, 
-    o;  // o is the output.
+    o;  //o is the output.
 function setup() {
     noCanvas();
     p = createP("Text Reverser program!");
     p.position(150);
-    t = createInput(""); t.position(150, p.position().y + 100);
+    t = createInput(""); t.position(150, p.position().y + 50);
     o = createInput(""); o.position(150, t.position().y + 25);
 }
 
